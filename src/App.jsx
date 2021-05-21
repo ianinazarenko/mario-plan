5f7a662d38c8cc1a03c0d71a27a5a50645772e44
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-05-21 13:25:16 +0300

    Set up project structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <header className='App-header'></header>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
